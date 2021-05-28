adf065a12b77b88e3774b8c00419f5bce58328f8
Adriano Amaricci <adriano.amaricci@gmail.com>
2021-05-28 09:39:45 +0200

    1.0.5 Code Release

--- src/VERSION.f90
+++ src/VERSION.f90
@@ -1,5 +1,5 @@
 MODULE ED_VERSION
   implicit none
   !GIT VERSION
-  character(len=41),parameter,public :: version = "95ee2834e6be21f8c0f03577b158ad6f928a6568"
+  character(len=41),parameter,public :: version = "48ba3ba8bf013a17b3c4062ff281342960c13740"
 END MODULE ED_VERSION
